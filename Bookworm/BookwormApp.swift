592268ff578d63c4f6610b11822c7a650fb4ac70
warmrome <pxpx786@gmail.com>
2021-12-02 12:56:32 -0800

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Jameson Hurst on 12/2/21.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
